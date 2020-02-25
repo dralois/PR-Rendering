57aeb2a8a59682fd01d0137dd2e8b77a03fb6f4a
dralois <epple.alexander@web.de>
2020-02-25 11:32:40 +0100

    Removed a few hints for opencv

--- cmake/FindOpenCV.cmake
+++ cmake/FindOpenCV.cmake
@@ -9,23 +9,14 @@ IF(OPENCV2_INCLUDE_DIR)
     SET(OPENCV2_FIND_QUIETLY TRUE)
 ENDIF(OPENCV2_INCLUDE_DIR)
 
-SET(OPENCV2_EXTERNLIBS $ENV{EXTERNLIBS}/opencv2/)
-
 FIND_PATH(OPENCV2_INCLUDE_DIR
     NAMES
     "cv.h"
     "opencv.hpp"
     HINTS
-    ${OPENCV2_EXTERNLIBS}/include
-    $ENV{OPENCV2_HOME}/include
-    ~/Library/Frameworks/include
-    /Library/Frameworks/include
-    /usr/local/include
     /usr/include
-    /sw/include # Fink
-    /opt/local/include # DarwinPorts
-    /opt/csw/include # Blastwave
-    /opt/include
+    /usr/local/include
+    /opt/local/include
     ${CMAKE_SOURCE_DIR}/includes
     ${CMAKE_SOURCE_DIR}/dependencies/opencv2/include
     ${PROJECT_SOURCE_DIR}/dependencies/opencv2/include
@@ -44,20 +35,11 @@ FIND_LIBRARY(OPENCV2_${_uppercomponent}_LIBRARY
     NAMES
     ${OPENCV2_NAMES}
     HINTS
-    ${OPENCV2_EXTERNLIBS}
-    $ENV{OPENCV2_HOME}
-    ${OPENCV2_EXTERNLIBS}/x64/vc14
-    ${OPENCV2_EXTERNLIBS}/x64/vc12
-    ${OPENCV2_EXTERNLIBS}/x64/vc11
-    ${OPENCV2_EXTERNLIBS}/x64/vc10
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local
-    /usr
-    /sw
-    /opt/local
-    /opt/csw
-    /opt
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    ${CMAKE_SOURCE_DIR}/lib
     ${CMAKE_SOURCE_DIR}/lib/release
     ${CMAKE_SOURCE_DIR}/dependencies/opencv2/lib/release
     ${PROJECT_SOURCE_DIR}/dependencies/opencv2/lib/release
@@ -74,11 +56,10 @@ IF(MSVC)
         NAMES
         ${OPENCV2_DBG_NAMES}
         HINTS
-        $ENV{OPENCV2_HOME}/lib
-        ${OPENCV2_EXTERNLIBS}/x64/vc14/lib
-        ${OPENCV2_EXTERNLIBS}/x64/vc12/lib
-        ${OPENCV2_EXTERNLIBS}/x64/vc11/lib
-        ${OPENCV2_EXTERNLIBS}/x64/vc10/lib
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
         ${CMAKE_SOURCE_DIR}/lib/debug
         ${CMAKE_SOURCE_DIR}/dependencies/opencv2/lib/debug
         ${PROJECT_SOURCE_DIR}/dependencies/opencv2/lib/debug
@@ -104,7 +85,11 @@ ELSE(MSVC)
         NAMES
         ${OPENCV2_WOV_NAMES}
         HINTS
-        $ENV{OPENCV2_HOME}/lib
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
+        ${CMAKE_SOURCE_DIR}/lib
     )
 
     SET(OPENCV2_LIBRARIES ${OPENCV2_LIBRARIES} ${OPENCV2_${_uppercomponent}_LIBRARY})
@@ -122,9 +107,9 @@ FIND_OPENCV2_COMPONENT(objdetect 2412)
 FIND_OPENCV2_COMPONENT(highgui 2412)
 FIND_OPENCV2_COMPONENT(imgproc 2412)
 
-if(OPENCV2_CORE_FOUND AND OPENCV2_OBJDETECT_FOUND AND OPENCV2_HIGHGUI_FOUND AND OPENCV2_IMGPROC_FOUND)
-    set(OPENCV2_FOUND TRUE)
-endif()
+IF(OPENCV2_CORE_FOUND AND OPENCV2_OBJDETECT_FOUND AND OPENCV2_HIGHGUI_FOUND AND OPENCV2_IMGPROC_FOUND)
+    SET(OPENCV2_FOUND TRUE)
+ENDIF()
 
 IF(OPENCV2_FOUND)
     SET(OPENCV2_INCLUDE_DIRS ${OPENCV2_INCLUDE_DIR})
