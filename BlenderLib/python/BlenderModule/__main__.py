a111b7e45a01228fa6b5ce2ef5e28a46af2c8471
dralois <epple.alexander@web.de>
2020-05-14 15:38:46 +0200

    Moved module test to package

---
+++ BlenderLib/python/BlenderModule/__main__.py
@@ -0,0 +1,27 @@
+import os
+import bpy
+import mathutils
+
+from . import *
+
+# Init
+Managers.Initialize(os.path.dirname(__file__), os.path.dirname(__file__))
+sceneMgr = Managers.SceneManager.SceneObjects()
+
+# Setup test scene
+cam = sceneMgr.AddCamera("cam")
+light = sceneMgr.AddLight("light")
+cube = sceneMgr.AddMesh("cube")
+
+# Create objects
+cam.CreateCamera(0.0, 0.0, 0.0, 0.0)
+light.CreatePointLight(100.0)
+cube.CreateCube(None)
+
+# Position objects in scene
+light.Position = mathutils.Vector((3, -4.2, 5))
+cam.Position = mathutils.Vector((6, -3, 5))
+cam.RotationEuler = mathutils.Euler((0.9, 0.0, 1.1))
+
+# Render
+sceneMgr.TryRenderNextCamera()
